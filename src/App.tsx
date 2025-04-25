4e3896efaca4fe10b2ca643a0122b882065cb793
HYEON <titeotty12@gmail.com>
2025-04-25 11:24:27 +0900

    feat: 폴더구조화 및 캘린더 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { HomePage } from '@/pages/HomePage';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="min-h-screen bg-gray-50">
-      <HomePage />
+    <div className="App">
+      <h1>Dailist Web</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
     </div>
   );
 }
